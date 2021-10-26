7fea4d0ec67ae3d516eafabd2ba3a3df35ff6ee7
Steffano Martinez <steffano1213@gmail.com>
2021-10-26 10:38:34 -0400

    Shit that doesnt work

---
+++ src/system_on_module/test_bench/test_bench.cpp
@@ -0,0 +1,20 @@
+#include "test_bench.h"
+
+void TestBench::begin()
+{
+    //* 1. Test External I/O
+
+    //* 2. Text Real Time Clock
+
+    //* 3. Test Internal Motion Unit (& calibrate?)
+
+    //* 4. Test Environmental Sensor
+
+    //* 5. Test eMMC
+
+    //* 6. Test Analog to Digital Converter
+
+    //* 7. CAN bus
+
+    //*
+}
---
+++ src/system_on_module/test_bench/test_bench.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Arduino.h>
+#include <esp32_utilities.h>
+
+class TestBench
+{
+public:
+    void begin();
+
+private:
+};
+
+extern TestBench testBench;
