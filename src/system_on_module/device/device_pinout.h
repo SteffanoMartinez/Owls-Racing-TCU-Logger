6da2b4ad0da8c9e0d460f4ba520317ae41e43a30
Steffano Martinez <steffano1213@gmail.com>
2021-10-26 13:26:43 -0400

    Device_Pinout Update

--- src/system_on_module/device/device_pinout.h
+++ src/system_on_module/device/device_pinout.h
@@ -17,6 +17,9 @@
 
 //********************************************************     APP GPIO     *********************************************************/
 
+//* MCU
+#define MCU_EN_PIN
+
 //* VSPI
 #define VSPI_MOSI_PIN 23
 #define VSPI_MISO_PIN 19
@@ -28,10 +31,44 @@
 #define HSPI_CLCK_PIN 16
 
 //* eMMC Memory
-#define eMMC0_EN_PIN -1
-#define eMMC0_CD_PIN 13
+#define eMMC0_EN_PIN_EXT 6
+#define eMMC0_CD_PIN_EXT 7
 
 //* CAN bus
 #define CAN0_CONTROLLER_CS_PIN 27
 #define CAN0_CONTROLLER_INT_PIN 35
+#define CAN0_CONTROLLER_EN_PIN_EXT 5
+
+//* ADC
+#define ADC0_EN_PIN_EXT 4
+
+//* IMU
+#define IMU_EN_PIN_EXT 8
+
+//* I2C_0
+#define I2C0_SDA_PIN 21
+#define I2C0_SCL 22
+
+//* I2C_1
+#define I2C1_SDA_PIN 4
+#define I2C1_SCL_PIN 5
+
+//* Internal GPIO's
+#define GPIO34_PIN 34
+#define GPIO26_PIN 26
+#define GPIO27_PIN 27
+#define GPIO12_PIN 12
+#define GPIO13_PIN 13
+
+//* External GPIO's
+#define GPIO10_PIN_EXT 10
+#define GPIO11_PIN_EXT 11
+#define GPIO12_PIN_EXT 12
+#define GPIO13_PIN_EXT 13
+#define GPIO14_PIN_EXT 14
+#define GPIO15_PIN_EXT 15
+
+//* Power
+#define P_5V_EN_PIN_EXT 9
+
 //* End.
